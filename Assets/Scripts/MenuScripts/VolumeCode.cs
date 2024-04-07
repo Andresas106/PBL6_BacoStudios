044b260c1a88d7f9288430f26749f1ba67b38b03
monroigggg <joel.monroig@estudiantat.upc.edu>
2024-04-07 19:07:06 +0200

    menus

--- Assets/Scripts/MenuScripts/VolumeCode.cs
+++ Assets/Scripts/MenuScripts/VolumeCode.cs
@@ -7,13 +7,13 @@ public class VolumeCode : MonoBehaviour
 {
     public Slider slider;
     public float sliderValue;
-    public Image imageMute;
+    //public Image imageMute;
     
     void Start()
     {
         slider.value = PlayerPrefs.GetFloat("audioVolume", 0.5f);
         AudioListener.volume = slider.value;
-        CheckMute();
+        //CheckMute();
     }
 
     public void ChangeSlider(float value)
@@ -21,7 +21,7 @@ public class VolumeCode : MonoBehaviour
         sliderValue = value;
         PlayerPrefs.SetFloat("audioVolume", sliderValue);
         AudioListener.volume = slider.value;
-        CheckMute();
+        //CheckMute();
     }
     
     public void CheckMute()
