850fd3ed2bc23a2bf7c91188ef9f0ea98252b1f3
Andreu <averdes12@gmail.com>
2024-04-08 11:34:57 +0200

    Timer + gameOver

--- Assets/Scripts/Timer/Timer.cs
+++ Assets/Scripts/Timer/Timer.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
     [SerializeField] private TMP_Text timertext;
     private float timeElapsed;
     private int minutes, seconds;
-    private float time = 90;
+    [SerializeField] private float time = 90;
+    [SerializeField] private GameObject gameOverMenu;
 
     // Start is called before the first frame update
     void Start()
@@ -19,15 +21,34 @@ public class Timer : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        timeElapsed -= Time.deltaTime;
-        minutes = (int)(timeElapsed / 60f);
-        seconds = (int)(timeElapsed - minutes * 60f);
+       if(timeElapsed >= 0)
+       {
+            Time.timeScale = 1;
+            timeElapsed -= Time.deltaTime;
+            minutes = (int)(timeElapsed / 60f);
+            seconds = (int)(timeElapsed - minutes * 60f);
 
-        timertext.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+            timertext.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+       }
+       else {
+            gameOverMenu.SetActive(true);
+            Time.timeScale = 0;
 
-        if(timeElapsed == 0.0f)
-        {
-            Debug.Log("GAME OVER");
-        }
+       }
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu"); 
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
