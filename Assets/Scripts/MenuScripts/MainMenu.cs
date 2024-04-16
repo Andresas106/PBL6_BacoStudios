aab1ac304e9cab35b17669460415be6e5744a1b3
Monroigggg <joel.monroig@estudiantat.upc.edu>
2024-04-16 13:26:54 +0200

    Menu!

--- Assets/Scripts/MenuScripts/MainMenu.cs
+++ Assets/Scripts/MenuScripts/MainMenu.cs
@@ -7,23 +7,92 @@ public class MainMenu : MonoBehaviour
 {
     [SerializeField] private GameObject optionsPanel;
     [SerializeField] private GameObject mainPanel;
+    [SerializeField] private GameObject menu1Panel;
+    [SerializeField] private GameObject menu2Panel;
+    [SerializeField] private GameObject colecPanel;
+    [SerializeField] private GameObject colec2Panel;
 
+    public int i;
+    public int c;
 
-    public void GameScene()
+    public void LoadLevel1() //Cargar nivel1
     {
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene("Level1_FigaFlawas");
     }
 
-    public void LoadOptions()
+    public void LoadLevel2() //Cargar nivel2
+    {
+        SceneManager.LoadScene("Level2_TheTyets");
+    }
+
+
+    public void LoadOptionsFromMenu()
     {
         optionsPanel.SetActive(true);
         mainPanel.SetActive(false);
     }
-
+    public void LoadOptionsFromMenu1()
+    {
+        
+        optionsPanel.SetActive(true);
+        menu1Panel.SetActive(false);
+        i = 1;
+    }
+    public void LoadOptionsFromMenu2()
+    {
+        
+        optionsPanel.SetActive(true);
+        menu2Panel.SetActive(false);
+        i = 2;
+    }
     public void BackToMenuFromOptions()
     {
-        optionsPanel.SetActive(false);
+        if (i == 1)
+        {
+            optionsPanel.SetActive(false);
+            menu1Panel.SetActive(true);
+        }
+        else if (i == 2)
+        {
+            optionsPanel.SetActive(false);
+            menu2Panel.SetActive(true);
+        }
+        else
+        {
+            optionsPanel.SetActive(false);
+            mainPanel.SetActive(true);
+        }
+    }
+    public void LoadColecFromMenu1()
+    {
+        colecPanel.SetActive(true);
+        menu1Panel.SetActive(false);
+        c = 1;
+    }
+    public void LoadColecFromMenu2()
+    {
+        colecPanel.SetActive(true);
+        menu2Panel.SetActive(false);
+        c = 2;
+    }
+    public void BackToMenuFromColec()
+    {
+        if (i == 1)
+        {
+            colecPanel.SetActive(false);
+            menu1Panel.SetActive(true);
+        }
+        else
+        {
+            colecPanel.SetActive(false);
+            menu2Panel.SetActive(true);
+        }
+    }
+    public void LoadMenu()
+    {
         mainPanel.SetActive(true);
+        menu1Panel.SetActive(false);
+        menu2Panel.SetActive(false);
     }
 
     public void Exit()
