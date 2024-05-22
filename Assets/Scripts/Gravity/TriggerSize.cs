e8d46fa616cb4436284c832dfa5adddfaa0cb433
Andresas <averdes12@gmail.com>
2024-05-22 21:10:47 +0200

    Modificado trigger oso nivel 1

---
+++ Assets/Scripts/Gravity/TriggerSize.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerSize : MonoBehaviour
+{
+    private CapsuleCollider myCollider;
+    private float originalSize;
+    private Vector3 originalCenter;
+
+    public float height = 0.4f;
+    public float centerC = 0.04f;
+        
+
+    void Start()
+    {
+        myCollider = GetComponent<CapsuleCollider>();
+        originalSize = myCollider.height;
+        originalCenter = myCollider.center;
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            StartCoroutine(FirstWaitTime());
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine(SecondWaitTime());
+        }
+    }
+
+
+    IEnumerator FirstWaitTime()
+    {
+        yield return new WaitForSeconds(1);
+
+        myCollider.height = height;
+        Vector3 center = myCollider.center;
+        center.y = centerC;
+        myCollider.center = center;
+
+    }
+
+    IEnumerator SecondWaitTime()
+    {
+        yield return new WaitForSeconds(1);
+
+        myCollider.height = originalSize;
+        myCollider.center = originalCenter;
+    }
+}
